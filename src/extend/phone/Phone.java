c19715ba38ab9bdb67d4540d212489fa44d6b577
hndwkwksn <hondawakuwakusan0630@gmail.com>
2022-10-07 13:12:58 +0900

    [Add]default

--- src/extend/phone/Phone.java
+++ src/extend/phone/Phone.java
@@ -1,6 +1,10 @@
 package extend.phone;
 
 public interface Phone {
-	int MAX_NUMBER_DIGITS = 11;
-	public void call(String number);
+	final int MAX_NUMBER_DIGITS = 11;
+	void call(String number);
+
+	default void powerOff() {
+		System.out.println("電源をきります。");
+	}
 }
