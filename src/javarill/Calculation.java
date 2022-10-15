e5e06526f54f59222e40484b20a84bddcca5ec34
hndwkwksn <hondawakuwakusan0630@gmail.com>
2022-10-15 18:43:40 +0900

    [Add]Calc

---
+++ src/javarill/Calculation.java
@@ -0,0 +1,13 @@
+package javarill;
+
+public class Calculation {
+
+	public static void main(String[] args) {
+		System.out.println("3.2+3=" + (3.2+3));
+		System.out.println("4-5=" + (4-5));
+		System.out.println("8.0/4=" + (8.0/4));
+		System.out.println("(3+2)*1.2+(3.2+3)*4=" + ((3+2)*1.2+(3.2+3)*4));
+
+	}
+
+}
