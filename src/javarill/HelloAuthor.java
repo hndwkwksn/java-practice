9199a085a6b1926ac50da013040cc551493a5953
hndwkwksn <hondawakuwakusan0630@gmail.com>
2022-10-15 18:32:23 +0900

    [Start]javarill

---
+++ src/javarill/HelloAuthor.java
@@ -0,0 +1,11 @@
+package javarill;
+
+public class HelloAuthor {
+
+	public static void main(String[] args) {
+		System.out.print("hello");
+		System.out.print("World");
+		System.out.println("Hello");
+		System.out.print("World");
+	}
+}
