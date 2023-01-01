c909def31c0f1981948929da87219dcebbb31a5c
hndwkwksn <hondawakuwakusan0630@gmail.com>
2023-01-01 11:17:57 +0900

    [Add]char

---
+++ src/practice/Char01.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Char01 {
+
+	public static void main(String[] args) {
+
+		char str1 = 'a';
+		System.out.println(str1);
+
+		char str2 = 97;
+		System.out.println(str2);
+	}
+
+}
