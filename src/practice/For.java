c9e6383cd463a29fb85fb2d6a3f1739376a68808
hndwkwksn <hondawakuwakusan0630@gmail.com>
2023-01-22 13:58:36 +0900

    [add]for

---
+++ src/practice/For.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class For {
+
+	public static void main(String[] args) {
+//		for(int i = 3; i < i++; i++) {
+//			int j = 0;
+//			while (j <= 3) {
+//				 System.out.print("a");
+//
+//			}
+//		}
+
+		for(int i= 0; i < 3; ++i ) {
+			System.out.println(i);
+		}
+	}
+
+}
